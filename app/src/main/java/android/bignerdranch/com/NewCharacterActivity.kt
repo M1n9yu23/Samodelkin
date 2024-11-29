8021535d487cfe0e87b44d40b6895df61adcc298
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-29 20:14:36 +0900

    [2024-11-29]

---
+++ app/src/main/java/android/bignerdranch/com/NewCharacterActivity.kt
@@ -0,0 +1,59 @@
+package android.bignerdranch.com
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+private const val CHARACTER_DATA_KEY = "CHARACTER_DATA_KEY"
+
+private var Bundle.characterData
+    get() = getSerializable(CHARACTER_DATA_KEY) as CharacterGenerator.CharacterData
+    set(value) = putSerializable(CHARACTER_DATA_KEY, value)
+
+class NewCharacterActivity : AppCompatActivity() {
+    private var characterData = CharacterGenerator.generate()
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.characterData = characterData // 이때 확장 속성의 set(characterData)가 실행된다.
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_character)
+
+        characterData = savedInstanceState?.characterData ?: CharacterGenerator.generate() // 이때 확장 속성의 get()이 실행된다.
+
+        val nameTextView = findViewById<TextView>(R.id.nameTextView)
+        val raceTextView = findViewById<TextView>(R.id.raceTextView)
+        val dexterityTextView = findViewById<TextView>(R.id.dexterityTextView)
+        val wisdomTextView = findViewById<TextView>(R.id.wisdomTextView)
+        val strengthTextView = findViewById<TextView>(R.id.strengthTextView)
+        val generateButton = findViewById<Button>(R.id.generateButton)
+
+        generateButton.setOnClickListener {
+            characterData = CharacterGenerator.generate()
+
+            characterData.run {
+                nameTextView.text = name
+                raceTextView.text = race
+                dexterityTextView.text = dex
+                wisdomTextView.text = wis
+                strengthTextView.text = str
+            }
+        }
+
+        characterData.run {
+            nameTextView.text = name
+            raceTextView.text = race
+            dexterityTextView.text = dex
+            wisdomTextView.text = wis
+            strengthTextView.text = str
+        }
+    }
+
+
+}
